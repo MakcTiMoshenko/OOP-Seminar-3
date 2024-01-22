04adfc044cfae5aeaa9b44e2b32e32940788148d
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2024-01-22 23:31:47 +0300

    Семинар 3
    Создать класс Поток содержащий в себе список УчебныхГрупп и реализующий интерфейс Iterable.
    Создать класс StreamComparator, реализующий сравнение количества групп входящих в Поток.
    Создать класс ПотокСервис, добавив в него метод сортировки списка потоков, используя созданный StreamComparator.
    Модифицировать класс Контроллер, добавив в него созданный сервис.
    Модифицировать класс Контроллер, добавив в него метод сортирующий список потоков, путем вызова созданного сервиса. Формат сдачи: ссылка на гитхаб-проект

---
+++ CourseServis.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class CourseServis {
+    public void sortCourses(List<Course> courses){
+        courses.sort(new StreamComparator());
+    }
+}
---
+++ StudentGroure.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class StudentGroure {
+    private String name;
+
+    public StudentGroure(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
